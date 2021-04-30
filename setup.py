c65384b0784458f603eef15d4679ce24dd0bfad5
Yusuke Niitani <yuyuniitani@gmail.com>
2021-04-30 13:56:40 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import find_packages, setup
 
 setup(
     name="seqtk",
-    version="0.0.1",
-    description="",
+    version="0.1.0",
+    description="seqtk provides various tools for Sequence.",
     author="Yusuke Niitani",
     author_email="",
     packages=find_packages(exclude=["tests"]),
     install_requires=[],
     package_data={"seqtk": ["py.typed"]},
+    url="https://github.com/yuyu2172/seqtk",
 )
